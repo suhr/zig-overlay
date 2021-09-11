5e7aab266e2cb933b54435386e999de9d8def7df
Michael Holmes <i.am@ceph3.us>
2021-09-11 01:52:40 +0100

    Add aarch64-darwin to supported archs

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
   };
   outputs = { self, nixpkgs, flake-utils, ... }:
     # List of systems where binaries are provided.
-    let systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" ];
+    let systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
     in flake-utils.lib.eachSystem systems (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in rec {
